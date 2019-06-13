9652f6f4ddb216d58551c514f3aa724d84cd89cb
mskalacki <mskalacki@gmail.com>
2019-06-13 23:56:46 +0200

    Hidden/shown card adding form feature.

--- app/Controllers/CardController.js
+++ app/Controllers/CardController.js
@@ -3,29 +3,27 @@ angular.module("app.Controllers").controller("CardController", function ($scope)
     $scope.TasksToDo =
         [
 
-            { name: 'Odrobić lekcje', id: '1' },
-            { name: 'Nakarmić kota', id: '2' },
-            { name: 'Zrobić zakupy', id: '3' }
+            { name: 'Odrobić lekcje', id: '1', content: "Odrobić z matematyki i angielskiego" },
+            { name: 'Nakarmić kota', id: '2', content: "Dać suche i mokre" },
+            { name: 'Zrobić zakupy', id: '3', content: "Mleko, mąka, sól, szampon, soki, piwo" }
 
         ];
 
         $scope.TasksDone = [
-            { name: 'Podlać kwiaty', id: '1' },
-            { name: 'Wynieść śmieci', id: '2' },
-            { name: 'Poodkurzać', id: '3' }
-
-
+            // { name: 'Podlać kwiaty', id: '1' },
+            // { name: 'Wynieść śmieci', id: '2' },
+            // { name: 'Poodkurzać', id: '3' }
         ]
     let idValue = 4;
-    $scope.AddCard = function () {
+    $scope.AddCard = function (name, content) {
 
-        let nameValue = document.querySelector('.Add-name').value;
+        //let nameValue = document.querySelector('.Add-name').value;
+       
 
-        if (nameValue) {
-            $scope.TasksToDo.push({ name: nameValue, id: idValue });
+        if (name) {
+            $scope.TasksToDo.push({ name: name, id: idValue, content: content });
 
             idValue++;
-
         }
 
 
@@ -35,13 +33,8 @@ angular.module("app.Controllers").controller("CardController", function ($scope)
        let index = $scope.TasksToDo.findIndex(x => x.name === taskName);
        let tempSplice = $scope.TasksToDo.splice(index,1);
        
-        // $scope.TasksDone.unshift($scope.TasksToDo[index]);
-        // $scope.TasksTodo.splice(0,1);
-        $scope.TasksDone.unshift(tempSplice[0]);
-
-      
-
-       
+    
+        $scope.TasksDone.push(tempSplice[0]); 
     }
 
 
